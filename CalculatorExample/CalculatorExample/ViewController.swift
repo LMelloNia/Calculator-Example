6dfe596c91b1f792a6972cdd9921e33467f415ba
LMelloNia <ch10152080@gmail.com>
2023-02-13 22:04:58 +0900

    기초적인 기능 구현
    간단한 계산기능 구현
    
    올바른 타입으로 입력 해야지만 계산이 됨 나머지는 오류, 입력 제한은 불가능

--- CalculatorExample/CalculatorExample/ViewController.swift
+++ CalculatorExample/CalculatorExample/ViewController.swift
@@ -9,8 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstNumberTextField: UITextField!
+    @IBOutlet weak var secondNumberTextField: UITextField!
+    @IBOutlet weak var operatorTextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var calculateButton: UIButton!
+//    var btnCount: Int = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+    }
+    
+    
+    @IBAction func calculate(_ sender: UIButton) {
+        var firstNum = Double(firstNumberTextField.text!)!
+        var secondNum = Double(secondNumberTextField.text!)!
+//        if btnCount % 2 == 0{
+            switch operatorTextField.text {
+            case .none:
+                break
+            case .some(let x):
+                switch x {
+                case "+":
+                    resultLabel.text = String(firstNum + secondNum)
+                case "-":
+                    resultLabel.text = String(firstNum - secondNum)
+                case "*":
+                    resultLabel.text = String(firstNum * secondNum)
+                case "/":
+                    resultLabel.text = String(firstNum / secondNum)
+                default:
+                    break
+                }
+            }
+//        }
     }
     //버튼 한번 = 계산 버튼 두번 =리셋
     //사칙연산 좌 중 우 (1 + 1)
