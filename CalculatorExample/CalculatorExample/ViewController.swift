9e043eb7b16e4f1cafe3ec2734f6806a9b0cac16
LMelloNia <ch10152080@gmail.com>
2023-02-13 21:19:40 +0900

    0213
    프로젝트 생성

---
+++ CalculatorExample/CalculatorExample/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  CalculatorExample
+//
+//  Created by 김시훈 on 2023/02/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    //버튼 한번 = 계산 버튼 두번 =리셋
+    //사칙연산 좌 중 우 (1 + 1)
+    //계산값 Label에 표시
+    // + 계산 히스토리
+    // 2/14
+
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Calculator-Example
+# CalculatorExample
